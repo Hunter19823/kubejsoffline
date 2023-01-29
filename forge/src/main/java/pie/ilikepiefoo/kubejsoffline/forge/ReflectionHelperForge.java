b882fef97af4f4d9db3c6547942a0362a2dfe028
Hunter19823 <hspragg@asu.edu>
2023-01-29 14:00:12 -0700

    Version 1.0.3: Fully functional on Forge!!!

--- forge/src/main/java/pie/ilikepiefoo/kubejsoffline/forge/ReflectionHelperForge.java
+++ forge/src/main/java/pie/ilikepiefoo/kubejsoffline/forge/ReflectionHelperForge.java
@@ -2,38 +2,37 @@ package pie.ilikepiefoo.kubejsoffline.forge;
 
 import dev.architectury.platform.Platform;
 import dev.latvian.mods.kubejs.event.EventJS;
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.FieldInfo;
+import javassist.bytecode.MethodInfo;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.loading.FMLPaths;
+import org.reflections.Configuration;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+import org.reflections.util.ConfigurationBuilder;
 import org.reflections.vfs.Vfs;
 import org.reflections.vfs.ZipDir;
 import pie.ilikepiefoo.kubejsoffline.util.ReflectionHelper;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Set;
-import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ReflectionHelperForge implements ReflectionHelper {
 	static {
 		Vfs.addDefaultURLTypes(new Vfs.UrlType() {
 			@Override
 			public boolean matches(URL url) {
-
 				return ("union".equals(url.getProtocol())) && !((url).toExternalForm().matches(".+\\.jar!/.+"));
 			}
 
 			@Override
 			public Vfs.Dir createDir(URL url) throws Exception {
-
-				String path = url.getPath();
+				String path = url.toURI().getPath();
 				if(path.indexOf('/') == 0) {
 					path = path.substring(1);
 				}
@@ -49,79 +48,40 @@ public class ReflectionHelperForge implements ReflectionHelper {
 				}
 			}
 		});
+		LOG.info("Added new union URL Type to VFS");
+		Vfs.addDefaultURLTypes(Vfs.DefaultUrlTypes.directory);
+		LOG.info("Added new directory Url Types to VFS");
+		LOG.info("Now attempting to load JavaAssist ClassFile class");
+		try {
+			ClassFile file = new ClassFile(false, "test", null);
+			file.addField(new FieldInfo(file.getConstPool(), "test", "I"));
+			file.addMethod(new MethodInfo(file.getConstPool(), "test", "()V"));
+			LOG.info("Successfully loaded JavaAssist ClassFile class!");
+		} catch (Throwable e) {
+			LOG.error("Failed to load JavaAssist ClassFile class. KubeJS Offline Functionality will break past this point!", e);
+		}
 	}
 
 	@Override
 	public Class[] getClasses() {
+		return useReflections();
+	}
+	private static Class[] useReflections() {
 		Package[] packages = Package.getPackages();
 		String[] packageNames = Arrays.stream(packages).parallel().map(Package::getName).toList().toArray(new String[0]);
-		Set<String> packageSet = Arrays.stream(packages).parallel().map(Package::getName).collect(Collectors.toSet());
-		LOG.info("Full Package List: " + packageSet);
-		return findModClasses()
-				.parallelStream()
-				.filter(name -> name.contains("."))
-				.filter(name -> (!Platform.getEnv().isClient() && !name.contains("client")))
-				.filter(name -> packageSet.parallelStream().anyMatch(name::startsWith))
-				.map(ReflectionHelperForge::findClass).toArray(Class[]::new);
-	}
-	private static Class<?> findClass(String name) {
+		URL modsFolder;
 		try {
-			return Class.forName(name);
-		} catch (Throwable ignored) {}
-		return null;
-	}
-
-	public static final List<String> findModClasses() {
-		List<String> output = new ArrayList<>();
-		// Open the mods folder
-		File modsFolder = FMLPaths.MODSDIR.get().toFile();
-		if (!modsFolder.exists()) {
-			return List.of();
+			modsFolder = Platform.getModsFolder().toUri().toURL();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
 		}
-		// Get all the files in the mods folder
-		File[] files = modsFolder.listFiles();
-		if (files == null) {
-			return List.of();
-		}
-		Stream.of(files)
-				.parallel()
-				.forEach(file -> output.addAll(searchFile(file)));
-		// Search for all the classes in the mods folder
-		return output;
-	}
-
-	public static List<String> searchFile(File file) {
-		if(file == null) return List.of();
-		if(!file.exists()) return List.of();
-		List<String> classes = new ArrayList<>();
-		if(file.isDirectory()) {
-			File[] files = file.listFiles();
-			if (files != null) {
-				for (File f : files) {
-					classes.addAll(searchFile(f));
-				}
-			}
-		} else if(file.getName().endsWith(".jar")) {
-			classes.addAll(searchJar(file));
-		}
-		return classes;
-	}
-
-	public static List<String> searchJar(File file) {
-		try (JarFile jarFile = new JarFile(file)){
-			Enumeration<JarEntry> entries = jarFile.entries();
-			List<String> classes = new ArrayList<>();
-			while (entries.hasMoreElements()) {
-				String name = entries.nextElement().getName();
-				if (name.endsWith(".class") && !name.contains("$")) {
-					classes.add(name.replace('/', '.').substring(0, name.length() - 6));
-				}
-			}
-			return classes;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return List.of();
+		Configuration configuration = new ConfigurationBuilder()
+				.setParallel(true)
+				.forPackages(packageNames)
+				.addUrls(modsFolder)
+				.setScanners(Scanners.SubTypes.filterResultsBy(pred->true), Scanners.Resources);
+		Reflections reflections = new Reflections(configuration);
+		return reflections.getSubTypesOf(Object.class).toArray(new Class[0]);
 	}
 
 	@Override
