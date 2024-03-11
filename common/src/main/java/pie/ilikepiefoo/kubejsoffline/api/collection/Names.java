311ea4ede784552053e25300a3e99784bf9443c5
Hunter19823 <hspragg@asu.edu>
2024-03-11 16:48:22 -0700

    Setup Names

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/collection/Names.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/collection/Names.java
@@ -1,4 +1,11 @@
 package pie.ilikepiefoo.kubejsoffline.api.collection;
 
+import java.util.NavigableSet;
+
 public interface Names {
+    NavigableSet<String> getAllNames();
+
+    boolean contains( String name );
+
+    boolean addName( String name );
 }
