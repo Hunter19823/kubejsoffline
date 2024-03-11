a9c7dc2882e8e5a0ce85eec213a3f55b1f35024d
Hunter19823 <hspragg@asu.edu>
2024-03-11 16:48:13 -0700

    Setup Annotations

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/collection/Annotations.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/collection/Annotations.java
@@ -1,4 +1,11 @@
 package pie.ilikepiefoo.kubejsoffline.api.collection;
 
+import java.util.NavigableSet;
+
 public interface Annotations {
+    NavigableSet<String> getAllAnnotations();
+
+    boolean contains( String annotation );
+
+    boolean addAnnotation( String annotation );
 }
