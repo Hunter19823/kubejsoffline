a8a1e9fbc670cc81734d576b088474e1363ef6d3
Hunter19823 <hspragg@asu.edu>
2024-03-12 16:29:40 -0700

    Implemented TypeVariableWrapper

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/impl/datastructure/TypeVariableWrapper.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/impl/datastructure/TypeVariableWrapper.java
@@ -1,42 +1,62 @@
 package pie.ilikepiefoo.kubejsoffline.impl.datastructure;
 
-import pie.ilikepiefoo.kubejsoffline.api.datastructure.IndexedData;
 import pie.ilikepiefoo.kubejsoffline.api.datastructure.TypeVariableData;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.NameID;
-import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeID;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeOrTypeVariableID;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeVariableID;
 import pie.ilikepiefoo.kubejsoffline.impl.CollectionGroup;
+import pie.ilikepiefoo.kubejsoffline.impl.TypeManager;
 
+import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.List;
 
 public class TypeVariableWrapper implements TypeVariableData {
+    protected final CollectionGroup collectionGroup;
+    protected final TypeVariable<?> typeVariable;
+    protected TypeVariableID index;
+    protected NameID name;
+    protected List<TypeOrTypeVariableID> bounds;
+
     public TypeVariableWrapper(CollectionGroup collectionGroup, TypeVariable<?> typeVariable) {
+        this.collectionGroup = collectionGroup;
+        this.typeVariable = typeVariable;
     }
 
     @Override
-    public TypeVariableID getIndex() {
-        return null;
-    }
-
-    @Override
-    public IndexedData<TypeID> setIndex(TypeID index) {
-        return null;
+    public TypeVariableData setIndex(TypeOrTypeVariableID index) {
+        if (!index.isTypeVariable()) {
+            throw new IllegalArgumentException("TypeVariableData can only be indexed by TypeVariableID");
+        }
+        this.index = index.asTypeVariable();
+        return this;
     }
 
     @Override
     public NameID getName() {
-        return null;
+        if (name != null) {
+            return name;
+        }
+        return name = collectionGroup.names().addName(typeVariable.getName());
     }
 
     @Override
-    public List<TypeOrTypeVariableID> getExtends() {
-        return null;
+    public List<TypeOrTypeVariableID> getBounds() {
+        if (bounds != null) {
+            return bounds;
+        }
+        List<TypeOrTypeVariableID> bounds = new java.util.LinkedList<>();
+        for (Type type : typeVariable.getBounds()) {
+            if (type == Object.class) {
+                continue;
+            }
+            bounds.add(TypeManager.INSTANCE.getID(type));
+        }
+        return this.bounds = bounds;
     }
 
     @Override
-    public List<TypeOrTypeVariableID> getSupers() {
-        return null;
+    public TypeVariableID getIndex() {
+        return index;
     }
 }
