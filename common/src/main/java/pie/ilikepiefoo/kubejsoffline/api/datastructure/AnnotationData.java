ffa1e60904f900a916301e9e9d61c93d30597b2a
Hunter19823 <hspragg@asu.edu>
2024-03-12 15:58:08 -0700

    Made AnnotationData extend IndexedData

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/AnnotationData.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/AnnotationData.java
@@ -1,8 +1,9 @@
 package pie.ilikepiefoo.kubejsoffline.api.datastructure;
 
+import pie.ilikepiefoo.kubejsoffline.api.identifier.AnnotationID;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeID;
 
-public interface AnnotationData {
+public interface AnnotationData extends IndexedData<AnnotationID> {
     TypeID getAnnotationType();
 
     String getAnnotationValue();
