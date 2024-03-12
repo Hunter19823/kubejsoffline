c50e1f93a1b726c9c1e4d7c2060fe7bcd3cc4961
Hunter19823 <hspragg@asu.edu>
2024-03-11 17:29:25 -0700

    Scaffolded ParameterizedTypeData

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/ParameterizedTypeData.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/ParameterizedTypeData.java
@@ -1,4 +1,12 @@
 package pie.ilikepiefoo.kubejsoffline.api.datastructure;
 
+import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeID;
+import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeOrTypeVariableID;
+
+import java.util.List;
+
 public interface ParameterizedTypeData {
+    TypeID getRawType();
+
+    List<TypeOrTypeVariableID> getActualTypeArguments();
 }
