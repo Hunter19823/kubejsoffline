c56ad070a0796da9f0ef024d9d5c76ad1072c3aa
Hunter19823 <hspragg@asu.edu>
2024-03-11 17:28:30 -0700

    Scaffolded ParameterData

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/ParameterData.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/ParameterData.java
@@ -1,5 +1,16 @@
 package pie.ilikepiefoo.kubejsoffline.api.datastructure;
 
-public interface ParameterData {
+import pie.ilikepiefoo.kubejsoffline.api.identifier.AnnotationID;
+import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeOrTypeVariableID;
 
+import java.util.List;
+
+public interface ParameterData {
+    int getModifiers();
+
+    String getName();
+
+    TypeOrTypeVariableID getType();
+
+    List<AnnotationID> getAnnotations();
 }
