64f6e9a59e1a9ebc16b9f4b3506a3983c1117c45
Hunter19823 <hspragg@asu.edu>
2024-03-11 17:21:25 -0700

    Updated Types

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/collection/Types.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/collection/Types.java
@@ -3,6 +3,8 @@ package pie.ilikepiefoo.kubejsoffline.api.collection;
 import pie.ilikepiefoo.kubejsoffline.api.datastructure.TypeData;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeID;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.WildcardType;
 import java.util.NavigableMap;
 
 public interface Types {
@@ -11,4 +13,10 @@ public interface Types {
     TypeID addType(TypeData data);
 
     boolean contains( TypeData data );
+
+    TypeID getID(Class<?> clazz);
+
+    TypeID getID(ParameterizedType type);
+
+    TypeID getID(WildcardType type);
 }
