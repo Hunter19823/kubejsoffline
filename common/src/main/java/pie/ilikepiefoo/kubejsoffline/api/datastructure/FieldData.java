5fd2e5ef96fa1f6dce2c8bc3d614fbd9bd3480a2
Hunter19823 <hspragg@asu.edu>
2024-03-11 17:13:02 -0700

    Scaffolded FieldData

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/FieldData.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/datastructure/FieldData.java
@@ -1,4 +1,17 @@
 package pie.ilikepiefoo.kubejsoffline.api.datastructure;
 
+import pie.ilikepiefoo.kubejsoffline.api.identifier.AnnotationID;
+import pie.ilikepiefoo.kubejsoffline.api.identifier.NameID;
+import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeOrTypeVariableID;
+
+import java.util.List;
+
 public interface FieldData {
+    NameID getName();
+
+    int getModifiers();
+
+    TypeOrTypeVariableID getType();
+
+    List<AnnotationID> getAnnotations();
 }
