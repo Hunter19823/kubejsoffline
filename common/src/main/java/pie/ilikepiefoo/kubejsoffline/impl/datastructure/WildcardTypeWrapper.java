f3d2dce2d45cfc23996ae248638d19be689d2f1f
Hunter19823 <hspragg@asu.edu>
2024-03-12 16:29:46 -0700

    Implemented WildcardTypeWrapper

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/impl/datastructure/WildcardTypeWrapper.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/impl/datastructure/WildcardTypeWrapper.java
@@ -1,34 +1,69 @@
 package pie.ilikepiefoo.kubejsoffline.impl.datastructure;
 
-import pie.ilikepiefoo.kubejsoffline.api.datastructure.IndexedData;
 import pie.ilikepiefoo.kubejsoffline.api.datastructure.WildcardTypeData;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeID;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeOrTypeVariableID;
 import pie.ilikepiefoo.kubejsoffline.impl.CollectionGroup;
+import pie.ilikepiefoo.kubejsoffline.impl.TypeManager;
 
+import java.lang.reflect.Type;
 import java.lang.reflect.WildcardType;
+import java.util.List;
 
 public class WildcardTypeWrapper implements WildcardTypeData {
+    protected final CollectionGroup collectionGroup;
+    protected final WildcardType wildcardType;
+    protected TypeID index;
+    protected List<TypeOrTypeVariableID> extendsBounds;
+    protected List<TypeOrTypeVariableID> superBounds;
+
+
     public WildcardTypeWrapper(CollectionGroup collectionGroup, WildcardType wildcardType) {
+        this.collectionGroup = collectionGroup;
+        this.wildcardType = wildcardType;
     }
 
     @Override
     public TypeID getIndex() {
-        return null;
+        return index;
     }
 
     @Override
-    public IndexedData<TypeID> setIndex(TypeID index) {
-        return null;
+    public WildcardTypeData setIndex(TypeOrTypeVariableID index) {
+        if (!index.isType()) {
+            throw new IllegalArgumentException("WildcardTypeData can only be indexed by TypeID");
+        }
+        this.index = index.asType();
+        return this;
     }
 
     @Override
-    public TypeOrTypeVariableID getExtends() {
-        return null;
+    public List<TypeOrTypeVariableID> getExtends() {
+        if (extendsBounds != null) {
+            return extendsBounds;
+        }
+        List<TypeOrTypeVariableID> extendsBounds = new java.util.LinkedList<>();
+        for (Type type : wildcardType.getUpperBounds()) {
+            if (type == Object.class) {
+                continue;
+            }
+            extendsBounds.add(TypeManager.INSTANCE.getID(type));
+        }
+        return this.extendsBounds = extendsBounds;
     }
 
     @Override
-    public TypeOrTypeVariableID getSuper() {
-        return null;
+    public List<TypeOrTypeVariableID> getSuper() {
+        if (superBounds != null) {
+            return superBounds;
+        }
+        List<TypeOrTypeVariableID> superBounds = new java.util.LinkedList<>();
+        for (Type type : wildcardType.getLowerBounds()) {
+            if (type == Object.class) {
+                continue;
+            }
+            superBounds.add(TypeManager.INSTANCE.getID(type));
+        }
+        return this.superBounds = superBounds;
     }
 }
