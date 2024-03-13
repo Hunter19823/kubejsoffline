a6485c5fa97574ee0158a5bbd0acb4f2f4f11820
Hunter19823 <hspragg@asu.edu>
2024-03-13 10:16:50 -0700

    Changed JSONSerializable to use Collection instead of list

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/JSONSerializable.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/api/JSONSerializable.java
@@ -3,10 +3,10 @@ package pie.ilikepiefoo.kubejsoffline.api;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface JSONSerializable {
-    static <S extends JSONSerializable> JsonArray of(List<S> jsonSerializableList) {
+    static <S extends JSONSerializable> JsonArray of(Collection<S> jsonSerializableList) {
         JsonArray jsonArray = new JsonArray();
         for (JSONSerializable jsonSerializable : jsonSerializableList) {
             jsonArray.add(jsonSerializable.toJSON());
