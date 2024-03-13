1da85bb9843ed69db2d72b1e750bc74b30f35b47
Hunter19823 <hspragg@asu.edu>
2024-03-13 10:11:27 -0700

    Implemented RawClassWrapper's toJSON

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/impl/datastructure/RawClassWrapper.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/impl/datastructure/RawClassWrapper.java
@@ -1,5 +1,8 @@
 package pie.ilikepiefoo.kubejsoffline.impl.datastructure;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import pie.ilikepiefoo.kubejsoffline.api.JSONSerializable;
 import pie.ilikepiefoo.kubejsoffline.api.datastructure.ConstructorData;
 import pie.ilikepiefoo.kubejsoffline.api.datastructure.FieldData;
 import pie.ilikepiefoo.kubejsoffline.api.datastructure.MethodData;
@@ -11,6 +14,7 @@ import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeID;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeOrTypeVariableID;
 import pie.ilikepiefoo.kubejsoffline.api.identifier.TypeVariableID;
 import pie.ilikepiefoo.kubejsoffline.impl.CollectionGroup;
+import pie.ilikepiefoo.kubejsoffline.util.json.JSONProperty;
 
 import java.util.List;
 
@@ -147,5 +151,46 @@ public class RawClassWrapper implements RawClassData {
     public RawClassData setIndex(TypeOrTypeVariableID index) {
         this.index = index.asType();
         return this;
+    }
+
+    @Override
+    public JsonElement toJSON() {
+        var json = new JsonObject();
+        json.add(JSONProperty.CLASS_NAME.jsName, getName().toJSON());
+        if (!getAnnotations().isEmpty()) {
+            json.add(JSONProperty.ANNOTATIONS.jsName, JSONSerializable.of(getAnnotations()));
+        }
+        json.addProperty(JSONProperty.MODIFIERS.jsName, getModifiers());
+        if (!getTypeParameters().isEmpty()) {
+            json.add(JSONProperty.TYPE_VARIABLES.jsName, JSONSerializable.of(getTypeParameters()));
+        }
+        if (getPackage() != null) {
+            json.add(JSONProperty.PACKAGE_NAME.jsName, getPackage().toJSON());
+        }
+        if (getSuperClass() != null) {
+            json.add(JSONProperty.SUPER_CLASS.jsName, getSuperClass().toJSON());
+        }
+        if (!getInterfaces().isEmpty()) {
+            json.add(JSONProperty.INTERFACES.jsName, JSONSerializable.of(getInterfaces()));
+        }
+        if (!getInnerClasses().isEmpty()) {
+            json.add(JSONProperty.INNER_CLASSES.jsName, JSONSerializable.of(getInnerClasses()));
+        }
+        if (getEnclosingClass() != null) {
+            json.add(JSONProperty.ENCLOSING_CLASS.jsName, getEnclosingClass().toJSON());
+        }
+        if (getDeclaringClass() != null) {
+            json.add(JSONProperty.DECLARING_CLASS.jsName, getDeclaringClass().toJSON());
+        }
+        if (!getFields().isEmpty()) {
+            json.add(JSONProperty.FIELDS.jsName, JSONSerializable.of(getFields()));
+        }
+        if (!getConstructors().isEmpty()) {
+            json.add(JSONProperty.CONSTRUCTORS.jsName, JSONSerializable.of(getConstructors()));
+        }
+        if (!getMethods().isEmpty()) {
+            json.add(JSONProperty.METHODS.jsName, JSONSerializable.of(getMethods()));
+        }
+        return json;
     }
 }
