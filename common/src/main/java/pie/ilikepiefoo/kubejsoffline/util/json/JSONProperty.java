338bb1beba9381ddca277759d1f5c4e53b5a3700
Hunter19823 <hspragg@asu.edu>
2024-03-13 14:11:27 -0700

    Started deprecating some soon-to-be removed properties

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/util/json/JSONProperty.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/util/json/JSONProperty.java
@@ -4,73 +4,95 @@ import com.google.gson.JsonObject;
 
 public enum JSONProperty {
     // Compression Tag Properties
+    @Deprecated
     VERSION("v"),
 
     // NEW DATA TAG Properties
+    @Deprecated
     TYPE_NAME("type_name"),
     TYPE_VARIABLES("type_variables"),
     TYPE_VARIABLE_NAME("type_variable_name"),
     TYPE_VARIABLE_BOUNDS("type_bounds"),
 
+    @Deprecated
     ARRAY_TYPE("array_type"),
+    @Deprecated
     ARRAY_NAME("array_name"),
 
     WILDCARD_TYPE("wildcard_type"),
     WILDCARD_UPPER_BOUNDS("wildcard_type"),
     WILDCARD_LOWER_BOUNDS("wildcard_lower_bounds"),
 
+    @Deprecated
     CLASS_REFERENCE("class_reference"),
+    @Deprecated
     CLASS_PARAMETERIZED_ARGUMENTS("class_parameterized_arguments"),
     CLASS_NAME("class_name"),
 
 
     // DATA TAG Properties
-    TYPE_ID("id"),
+    @Deprecated
+    TYPE_ID("type_id"),
+    @Deprecated
     TYPE_IDENTIFIER("type_identifier"),
     RAW_CLASS("raw_class"),
+    @Deprecated
     OUTER_CLASS("outer_class"),
     SUPER_CLASS("super_class"),
+    @Deprecated
     GENERIC_SUPER_CLASS("S"),
-    INTERFACES("i"),
+    INTERFACES("interfaces"),
+    @Deprecated
     GENERIC_INTERFACES("I"),
-    PACKAGE_NAME("P"),
+    PACKAGE_NAME("package_name"),
     ANNOTATIONS("annotations"),
     MODIFIERS("modifiers"),
     CONSTRUCTORS("constructors"),
     FIELDS("fields"),
     METHODS("methods"),
     PARAMETERS("parameters"),
+    @Deprecated
     VALUE("raw_value"),
 
+    @Deprecated
     ARRAY_DEPTH("depth"),
+    @Deprecated
     PARAMETERIZED_ARGUMENTS(PARAMETERS.jsName),
-    RAW_PARAMETERIZED_TYPE("r"),
-    OWNER_TYPE("o"),
+    RAW_PARAMETERIZED_TYPE("raw_parameterized_type"),
+    OWNER_TYPE("owner_type"),
 
-    PARAMETER_NAME(RAW_CLASS.jsName),
-    PARAMETER_TYPE(TYPE_ID.jsName),
-    PARAMETER_ANNOTATIONS(ANNOTATIONS.jsName),
+    PARAMETER_NAME("parameter_name"),
+    PARAMETER_TYPE("parameter_type"),
+    @Deprecated
+    PARAMETER_ANNOTATIONS("parameter_annotations"),
 
-    CONSTRUCTOR_NAME(RAW_CLASS.jsName),
+    @Deprecated
     CONSTRUCTOR_ANNOTATIONS(ANNOTATIONS.jsName),
 
-    METHOD_NAME(RAW_CLASS.jsName),
+    METHOD_NAME("method_name"),
+    @Deprecated
     METHOD_ANNOTATIONS(ANNOTATIONS.jsName),
-    METHOD_RETURN_TYPE(TYPE_ID.jsName),
+    METHOD_RETURN_TYPE("method_return_type"),
 
-    FIELD_NAME(RAW_CLASS.jsName),
+    FIELD_NAME("field_name"),
+    @Deprecated
     FIELD_ANNOTATIONS(ANNOTATIONS.jsName),
-    FIELD_TYPE(TYPE_ID.jsName),
+    FIELD_TYPE("field_type"),
+    @Deprecated
     FIELD_VALUE(VALUE.jsName),
 
 
+    @Deprecated
     EXECUTABLE_VALUE(VALUE.jsName),
+    @Deprecated
     EXECUTABLE_NAME("name"),
+    @Deprecated
     EXECUTABLE_ANNOTATIONS(ANNOTATIONS.jsName),
+    @Deprecated
     EXECUTABLE_TYPE(TYPE_ID.jsName),
 
-    ANNOTATION_TYPE(TYPE_ID.jsName),
-    ANNOTATION_STRING("s"),
+    ANNOTATION_TYPE("annotation_type"),
+    ANNOTATION_STRING("annotation_string"),
 
     BINDING_TYPE("btype"),
 
