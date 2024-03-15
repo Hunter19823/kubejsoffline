f2b4c8a2e1598dc2fd9a41b170b21d681bc29aab
Hunter19823 <hspragg@asu.edu>
2024-03-14 17:11:48 -0700

    Added some logging messages to ReflectionHelperFabric to mark Reflection Helper's progress

--- fabric/src/main/java/pie/ilikepiefoo/kubejsoffline/fabric/ReflectionHelperFabric.java
+++ fabric/src/main/java/pie/ilikepiefoo/kubejsoffline/fabric/ReflectionHelperFabric.java
@@ -3,6 +3,8 @@ package pie.ilikepiefoo.kubejsoffline.fabric;
 import dev.latvian.mods.kubejs.event.EventJS;
 import dev.latvian.mods.kubejs.recipe.RecipeJS;
 import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.reflections.Configuration;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
@@ -13,15 +15,23 @@ import java.nio.file.Path;
 import java.util.Arrays;
 
 public class ReflectionHelperFabric implements ReflectionHelper {
+    public static final Logger LOG = LogManager.getLogger();
+
     @Override
     public Class[] getClasses() {
+        LOG.info("Loading all packages...");
         Package[] packages = Package.getPackages();
+        LOG.info("Retrieved {} packages...", packages.length);
         String[] packageNames = Arrays.stream(packages).parallel().map(Package::getName).toList().toArray(new String[0]);
+        LOG.info("Finished mapping all package names...");
         Configuration configuration = new ConfigurationBuilder()
                 .setParallel(true)
                 .forPackages(packageNames)
                 .setScanners(Scanners.SubTypes.filterResultsBy(pred -> true), Scanners.Resources);
+        LOG.info("Finished building configuration...");
         Reflections reflections = new Reflections(configuration);
+        LOG.info("Finished building reflections...");
+        LOG.info("Now retrieving all SubTypes...");
         return reflections.getSubTypesOf(Object.class).toArray(new Class[0]);
     }
 
