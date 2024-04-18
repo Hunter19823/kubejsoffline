9210112990a403c8a065611fb92cd918f3cb4287
Hunter19823 <hspragg@asu.edu>
2024-04-17 18:37:24 -0700

    Updated default relationship setting

--- common/src/main/resources/assets/kubejsoffline/html/js/constants.js
+++ common/src/main/resources/assets/kubejsoffline/html/js/constants.js
@@ -54,7 +54,7 @@ const GLOBAL_SETTINGS = {
     "showPrivate": false,
     "showProtected": true,
     "showPackage": true,
-    "showRelationships": false,
+    "showRelationships": true,
     "defaultSearchPageSize": 25,
     "debug": false,
 };
