bebe97bf628425b6b4ab2d7bc0d33dec1addb66e
Hunter19823 <hspragg@asu.edu>
2025-03-29 23:58:28 -0700

    Tried to add Binding support.

--- neoforge/src/main/java/pie/ilikepiefoo/kubejsoffline/neoforge/DocumentationThread.java
+++ neoforge/src/main/java/pie/ilikepiefoo/kubejsoffline/neoforge/DocumentationThread.java
@@ -1,19 +1,12 @@
 package pie.ilikepiefoo.kubejsoffline.neoforge;
 
-import com.google.gson.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.Nullable;
-import pie.ilikepiefoo.kubejsoffline.core.DocumentationGenerator;
 import pie.ilikepiefoo.kubejsoffline.core.api.DocumentationBridge;
-import pie.ilikepiefoo.kubejsoffline.core.html.tag.Tag;
-import pie.ilikepiefoo.kubejsoffline.core.util.json.BindingsJSON;
+import pie.ilikepiefoo.kubejsoffline.core.api.DocumentationProvider;
+import pie.ilikepiefoo.kubejsoffline.core.impl.SimpleDocumentationProvider;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
 public class DocumentationThread extends Thread {
@@ -27,20 +20,6 @@ public class DocumentationThread extends Thread {
 
     }
 
-    @Nullable
-    private static File writeHTMLPage(final Tag<?> content) {
-        final File output = getFile();
-
-        try (final Writer writer = new FileWriter(output, StandardCharsets.UTF_8)) {
-            content.writeHTML(writer);
-            writer.flush();
-        } catch (final IOException e) {
-            LOG.error("Failed to write file: index.html to {}", output.getPath(), e);
-            return null;
-        }
-        return output;
-    }
-
     @SuppressWarnings("ResultOfMethodCallIgnored")
     private static File getFile() {
         final Path outputPath = getOutputPath().toAbsolutePath();
@@ -52,14 +31,14 @@ public class DocumentationThread extends Thread {
     }
 
     private static Path getOutputPath() {
-        return pie.ilikepiefoo.kubejsoffline.neoforge.KubeJSOffline.WORKING_DIR.resolve("kubejs/documentation");
+        return KubeJSOffline.WORKING_DIR.resolve("kubejs/documentation");
     }
 
     @Override
     public void run() {
         // Wait for Helper to be initialized.
         LOG.info("Starting Documentation Thread...");
-        while (null == pie.ilikepiefoo.kubejsoffline.neoforge.KubeJSOffline.HELPER) {
+        while (null == KubeJSOffline.HELPER) {
             LOG.info("Documentation Thread idling until Helper becomes available...");
             try {
                 this.wait(5000);
@@ -68,11 +47,11 @@ public class DocumentationThread extends Thread {
             }
         }
         LOG.info("Helper is available, now finding bindings...");
-        JsonObject bindings = new JsonObject();
-//        for (Map.Entry<ScriptType, JsonObject> entry : FakeBindingsEvent.bindingsJSON.entrySet()) {
-//            bindings.add(entry.getKey().name(), entry.getValue());
-//        }
-        BindingsJSON.setJsonObject(bindings);
-        DocumentationGenerator.generateHtmlPage(pie.ilikepiefoo.kubejsoffline.neoforge.KubeJSOffline.HELPER, bridge, new pie.ilikepiefoo.kubejsoffline.neoforge.RhinoTypeMapper(), getFile());
+        DocumentationProvider provider = new SimpleDocumentationProvider.Builder()
+                .setReflectionHelper(KubeJSOffline.HELPER)
+                .setDocumentationBridge(bridge)
+                .setBindingsProvider(new OfflinePlugin())
+                .build();
+        provider.generateDocumentation(getFile());
     }
 }
--- neoforge/src/main/java/pie/ilikepiefoo/kubejsoffline/neoforge/OfflinePlugin.java
+++ neoforge/src/main/java/pie/ilikepiefoo/kubejsoffline/neoforge/OfflinePlugin.java
@@ -1,8 +1,138 @@
 package pie.ilikepiefoo.kubejsoffline.neoforge;
 
 
+import com.google.gson.JsonElement;
+import dev.latvian.mods.kubejs.event.EventGroupWrapper;
+import dev.latvian.mods.kubejs.event.EventHandler;
 import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingRegistry;
+import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.rhino.NativeJavaObject;
+import dev.latvian.mods.rhino.ScriptableObject;
+import pie.ilikepiefoo.kubejsoffline.core.api.context.Binding;
+import pie.ilikepiefoo.kubejsoffline.core.api.context.BindingsProvider;
+import pie.ilikepiefoo.kubejsoffline.core.impl.context.SimpleBinding;
 
-public class OfflinePlugin implements KubeJSPlugin {
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OfflinePlugin implements KubeJSPlugin, BindingsProvider {
+    public static final Map<String, SimpleBinding.Builder> BINDING_MAP = new HashMap<>();
+    private final EnumMap<ScriptType, Set<BindingRegistry>> REGISTRIES = new EnumMap<>(ScriptType.class);
+
+    @Override
+    public Iterable<Binding> getBindings() {
+        for (Map.Entry<ScriptType, Set<BindingRegistry>> entry : REGISTRIES.entrySet()) {
+            if (entry.getValue() == null) {
+                continue;
+            }
+            Set<BindingRegistry> registries = entry.getValue();
+            for (BindingRegistry registry : registries) {
+                if (registry == null) {
+                    continue;
+                }
+                var objectIds = ScriptableObject.getPropertyIds(registry.context(), registry.scope());
+                for (Object objectId : objectIds) {
+                    if (objectId == null) {
+                        continue;
+                    }
+                    if (objectId instanceof String namedProperty) {
+                        var property = ScriptableObject.getProperty(registry.scope(), namedProperty, registry.context());
+                        addBinding(namedProperty, property, registry.context().getType());
+                    }
+                }
+            }
+        }
+        List<Binding> result = BINDING_MAP.values().stream().map(SimpleBinding.Builder::build).collect(Collectors.toList());
+        BINDING_MAP.clear();
+        REGISTRIES.clear();
+        return result;
+    }
+
+    public void addBinding(String name, Object value, ScriptType scriptType) {
+        if (name == null || value == null) {
+            return;
+        }
+        if (value instanceof NativeJavaObject nativeJavaObject) {
+            value = nativeJavaObject.unwrap();
+        }
+        try {
+            if (value instanceof EventGroupWrapper eventGroup) {
+                List<dev.latvian.mods.kubejs.event.EventHandler> eventHandlers =
+                        eventGroup
+                                .keySet()
+                                .stream()
+                                .map(eventGroup::get)
+                                .filter(event -> event instanceof dev.latvian.mods.kubejs.event.EventHandler)
+                                .map(event -> (dev.latvian.mods.kubejs.event.EventHandler) event)
+                                .toList();
+                for (EventHandler handler : eventHandlers) {
+                    String uniqueName =
+                            handler.target == null ?
+                                    String.format("%s.%s(%s)", name, handler.name, handler.eventType.get().getSimpleName()) :
+                                    String.format(
+                                            "%s.%s(%s, %s)",
+                                            name,
+                                            handler.name,
+                                            handler.eventType.get().getSimpleName(),
+                                            handler.target.describeType == null ?
+                                                    "???" : handler.target.describeType.toString()
+                                    );
+                    if (BINDING_MAP.containsKey(uniqueName)) {
+                        BINDING_MAP.get(uniqueName).addScope(scriptType.name);
+                        continue;
+                    }
+                    SimpleBinding.Builder handlerBuilder = SimpleBinding.Builder.from(uniqueName, handler.eventType.get());
+                    if (handler.target != null) {
+                        handlerBuilder.setData(handler.target.type);
+                    }
+                    handlerBuilder.addScope(scriptType.name);
+                    BINDING_MAP.put(uniqueName, handlerBuilder);
+                }
+            }
+            getBinding(name, value).addScope(scriptType.name);
+        } catch (Exception e) {
+            LOG.error("Failed to add binding: {} of type {}. Error: {}", name, value.getClass().getName(), e.getMessage());
+        }
+    }
+
+    public SimpleBinding.Builder getBinding(String name, Object value) {
+        if (value == null || name == null) {
+            return null;
+        }
+        if (BINDING_MAP.containsKey(name)) {
+            return BINDING_MAP.get(name);
+        }
+        LOG.info("Binding Found: {} of type {}", name, value.getClass().getName());
+        SimpleBinding.Builder builder;
+
+        switch (value) {
+            case Class<?> clazz -> {
+                builder = SimpleBinding.Builder.from(name, (Type) value);
+                if (clazz.isEnum()) {
+                    builder.setData(Arrays.stream(clazz.getEnumConstants()).map(Enum.class::cast).map(Enum::name).toArray());
+                }
+            }
+            case JsonElement jsonObject -> builder = SimpleBinding.Builder.from(name, jsonObject.getClass()).setData(jsonObject);
+            case Enum<?> enumValue -> builder = SimpleBinding.Builder.from(name, enumValue.getDeclaringClass()).setData(enumValue.name());
+            default -> builder = SimpleBinding.Builder.from(name, value.getClass()).setData(value);
+        }
+
+        BINDING_MAP.put(name, builder);
+        return builder;
+    }
+
+    @Override
+    public void registerBindings(BindingRegistry bindings) {
+        REGISTRIES.computeIfAbsent(bindings.context().getType(), t -> new HashSet<>());
+        REGISTRIES.get(bindings.context().getType()).add(bindings);
+    }
 
 }
