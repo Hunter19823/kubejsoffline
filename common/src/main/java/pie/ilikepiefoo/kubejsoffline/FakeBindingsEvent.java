616e8824d545bf62c7f0a4767c933823cbcf83b4
Hunter19823 <hspragg@asu.edu>
2025-03-29 22:37:54 -0700

    Added bindings to homepage (v5.0.2)

--- common/src/main/java/pie/ilikepiefoo/kubejsoffline/FakeBindingsEvent.java
+++ common/src/main/java/pie/ilikepiefoo/kubejsoffline/FakeBindingsEvent.java
@@ -1,18 +1,36 @@
 package pie.ilikepiefoo.kubejsoffline;
 
-import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
 import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.util.KubeJSPlugins;
 import dev.latvian.mods.rhino.BaseFunction;
 import dev.latvian.mods.rhino.util.DynamicFunction;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pie.ilikepiefoo.kubejsoffline.core.api.context.Binding;
+import pie.ilikepiefoo.kubejsoffline.core.api.context.BindingsProvider;
+import pie.ilikepiefoo.kubejsoffline.core.impl.context.SimpleBinding;
 
-import java.util.EnumMap;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class FakeBindingsEvent extends BindingsEvent {
-    public static final EnumMap<ScriptType, JsonObject> bindingsJSON = new EnumMap<>(ScriptType.class);
+public class FakeBindingsEvent extends BindingsEvent implements BindingsProvider {
+    public static final Map<String, SimpleBinding.Builder> BINDING_MAP = new HashMap<>();
+    private static final Logger LOG = LogManager.getLogger();
+    public ScriptType type;
 
-    public FakeBindingsEvent(FakeScriptManager manager) {
-        super(manager, null);
+
+    public FakeBindingsEvent() {
+        this(ScriptType.STARTUP);
+    }
+
+    public FakeBindingsEvent(ScriptType scriptType) {
+        super(new FakeScriptManager(scriptType), null);
     }
 
     @Override
@@ -21,78 +39,74 @@ public class FakeBindingsEvent extends BindingsEvent {
     }
 
     @Override
+    public Iterable<Binding> getBindings() {
+        for (ScriptType type : ScriptType.values()) {
+            FakeBindingsEvent event = new FakeBindingsEvent(type);
+            event.setType(type);
+            KubeJSPlugins.forEachPlugin(plugin -> plugin.addBindings(event));
+            BindingsEvent.EVENT.invoker().accept(event);
+        }
+        List<Binding> result = BINDING_MAP.values().stream().map(SimpleBinding.Builder::build).collect(Collectors.toList());
+        BINDING_MAP.clear();
+        return result;
+    }
+
+    public void setType(ScriptType type) {
+        this.type = type;
+    }    @Override
     public void add(String name, Object value) {
-//        if (value == null) {
-//            return;
-//        }
-//        if (value instanceof BaseFunction || value instanceof DynamicFunction.Callback) {
-//            return;
-//        }
-//
-//        var obj = bindingsJSON.computeIfAbsent(type, t -> new JsonObject());
-//        if (obj.has(name)) {
-//            return;
-//        }
-//        var temp = new JsonObject();
-//        obj.add(name, temp);
-//        obj = temp;
-//        obj.addProperty(JSONProperty.TYPE_IDENTIFIER.jsName, ClassJSONManager.getInstance().getTypeID(value.getClass()));
-//        if (value instanceof Map) {
-//            obj.addProperty(JSONProperty.BINDING_TYPE.jsName, JSONProperty.BINDING_TYPE_MAP.jsName);
-//            obj.addProperty(JSONProperty.BINDING_STRING.jsName, value.toString());
-//            return;
-//        }
-//        if (value instanceof Number || value instanceof Boolean || value instanceof String) {
-//            obj.addProperty(JSONProperty.BINDING_TYPE.jsName, JSONProperty.BINDING_TYPE_PRIMITIVE.jsName);
-//            obj.addProperty(JSONProperty.BINDING_STRING.jsName, value.toString());
-//            return;
-//        }
-//        if (value instanceof Enum<?> enumValue) {
-//            obj.addProperty(JSONProperty.BINDING_TYPE.jsName, JSONProperty.BINDING_TYPE_ENUM.jsName);
-//            obj.addProperty(JSONProperty.BINDING_STRING.jsName, enumValue.name());
-//            return;
-//        }
-//        if (value instanceof Class<?> clazz) {
-//            if (clazz.isEnum()) {
-//                obj.addProperty(JSONProperty.BINDING_TYPE.jsName, JSONProperty.BINDING_TYPE_ENUM.jsName);
-//            } else {
-//                obj.addProperty(JSONProperty.BINDING_TYPE.jsName, JSONProperty.BINDING_TYPE_CLASS.jsName);
-//            }
-//        }
+        if (name == null || value == null) {
+            return;
+        }
+        try {
+            getBinding(name, value).addScope(type.name);
+        } catch (Exception e) {
+            LOG.error("Failed to add binding: {} of type {}. Error: {}", name, value.getClass().getName(), e.getMessage());
+        }
     }
 
     @Override
     public void addFunction(String name, DynamicFunction.Callback callback) {
-//        getData(name, JSONProperty.BINDING_FUNCTION.jsName);
+        add(name, callback);
     }
 
     @Override
     public void addFunction(String name, DynamicFunction.Callback callback, Class<?>... types) {
-//        var obj = getData(name, JSONProperty.BINDING_FUNCTION.jsName);
-//        if (obj == null) {
-//            return;
-//        }
-//        var temp = new JsonArray();
-//        for (var param : types) {
-//            temp.add(ClassJSONManager.getInstance().getTypeID(param));
-//        }
-//        obj.add(JSONProperty.PARAMETERS.jsName, temp);
+        add(name, types);
     }
 
     @Override
     public void addFunction(String name, BaseFunction function) {
-//        getData(name, JSONProperty.BINDING_FUNCTION.jsName);
+        add(name, function);
     }
 
-//    private JsonObject getData(String name, String type) {
-//        var obj = bindingsJSON.computeIfAbsent(this.type, t -> new JsonObject());
-//        if (obj.has(name)) {
-//            return null;
-//        }
-//        var temp = new JsonObject();
-//        obj.add(name, temp);
-//        obj = temp;
-//        obj.addProperty(JSONProperty.BINDING_TYPE.jsName, type);
-//        return obj;
-//    }
+    public SimpleBinding.Builder getBinding(String name, Object value) {
+        if (value == null || name == null) {
+            return null;
+        }
+        if (BINDING_MAP.containsKey(name)) {
+            return BINDING_MAP.get(name);
+        }
+        LOG.info("Binding Found: {} of type {}", name, value.getClass().getName());
+        SimpleBinding.Builder builder;
+
+        if (value instanceof Class<?> clazz) {
+            builder = SimpleBinding.Builder.from(name, (Type) value);
+            if (clazz.isEnum()) {
+                builder.setData(Arrays.stream(clazz.getEnumConstants()).map(Enum.class::cast).map(Enum::name).toArray());
+            }
+        } else if (value instanceof JsonElement jsonObject) {
+            builder = SimpleBinding.Builder.from(name, jsonObject.getClass()).setData(jsonObject);
+        } else if (value instanceof Enum<?> enumValue) {
+            builder = SimpleBinding.Builder.from(name, enumValue.getDeclaringClass()).setData(enumValue.name());
+        } else {
+            builder = SimpleBinding.Builder.from(name, value.getClass()).setData(value);
+        }
+
+        BINDING_MAP.put(name, builder);
+        return builder;
+    }
+
+
+
 }
