728a9e0ef4d71893a5662c2d20d022ea3b0388a1
Hunter19823 <hspragg@asu.edu>
2025-08-16 15:15:39 -0700

    [RELEASE] Update version format to have build included in release version.

--- common/src/main/resources/html/js/constants.js
+++ common/src/main/resources/html/js/constants.js
@@ -16,11 +16,12 @@ const LOOK_UP_CACHE = new Map();
 const PROJECT_INFO = {
     "mod_id": "${mod_id}",
     "mod_name": "${mod_name}",
-    "mod_version": "${mod_version}",
+    "mod_version": "${version}",
     "mod_description": "${mod_description}",
     "mod_homepage": "${mod_homepage}",
     "mod_source": "${mod_source}",
     "mod_author": "${mod_author}",
+    "major_version": "${mod_version}",
     "curseforge_id": "${curseforge_id}",
     "modrinth_id": "${modrinth_id}",
     "minecraft_version": "${minecraft_version}",
